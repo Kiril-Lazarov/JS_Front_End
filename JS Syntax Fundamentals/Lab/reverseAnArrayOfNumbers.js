08c0780813f835c9e49261b441143ce5538d8f34
Kiril Lazarov <kirillazarov@abv.bg>
2023-02-25 05:11:26 +0200

    Task.

--- JS Syntax Fundamentals/Lab/reverseAnArrayOfNumbers.js
+++ JS Syntax Fundamentals/Lab/reverseAnArrayOfNumbers.js
@@ -1,4 +1,8 @@
-function reversearray (number, array) {
+function reverseArray (number, array) {
    
-    let partialArray = array[number-1]
+    let partialArray = array.slice(0,number)
+    partialArray.reverse()
+    console.log(partialArray.join(' '))
 }
+
+reverseArray(4, [-1, 20, 99, 5])
