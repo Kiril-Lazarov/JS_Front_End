0f86586ff474d63a07797bc9dead9915aa548e3e
Kiril Lazarov <kirillazarov@abv.bg>
2023-03-15 17:07:57 +0200

    Task.

---
+++ Functions and Statements/Exercices/add-and-subtract.js
@@ -0,0 +1,10 @@
+function addAndSubtract(num1,num2,num3){
+    console.log(sum(num1,num2) - num3);
+    function sum(num1, num2){
+        return num1 + num2;
+    }
+}
+
+
+
+addAndSubtract(23,6,10)
