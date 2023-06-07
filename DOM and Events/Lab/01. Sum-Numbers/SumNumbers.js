336301176ce5e89b7fc352c371266ce7549d15cb
Kiril Lazarov <kirillazarov@abv.bg>
2023-06-07 21:15:29 +0300

    Task

---
+++ DOM and Events/Lab/01. Sum-Numbers/SumNumbers.js
@@ -0,0 +1,5 @@
+function calc() {
+   const firstNum = document.getElementById('num1').value;
+   const secondNum = document.getElementById('num2').value;
+   document.getElementById('sum').value = Number(firstNum)+ Number(secondNum);
+}
