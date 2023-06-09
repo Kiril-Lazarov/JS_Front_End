3037eeb065ed38d83304a36ce363c4777ab824dd
Kiril Lazarov <kirillazarov@abv.bg>
2023-06-09 21:09:54 +0300

    Task

---
+++ DOM and Events/Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let moreContent = document.getElementById('more');
+    moreContent.style.display = 'none';
+    let newContent = document.getElementById('text');
+    newContent.style.display = 'inline';
+
+}
